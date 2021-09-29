2806f3dedae1c84cd11314f94ca5af9b65e41e0e
hahmsongmin <78192018+hahmsongmin@users.noreply.github.com>
2021-09-29 13:20:58 +0900

    TodoList 추가, 완료 체크 👏

--- src/components/todolist/todolist.jsx
+++ src/components/todolist/todolist.jsx
@@ -1,26 +1,66 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./todolist.module.css";
 
+let buttonIcon;
+
 const Todolist = () => {
+  const checkBoxRef = useRef();
   const inputRef = useRef();
-  const [todoInput, setTodoInput] = useState();
+
+  useEffect(() => {
+    checkBoxRef.current.addEventListener("click", itemClick);
+    buttonIcon = document.querySelector(".fa-plus");
+  }, []);
 
   const onClick = () => {
     inputRef.current.classList.toggle(`${styles.hidden}`);
+    const ok = inputRef.current.classList.contains(`${styles.hidden}`);
+    if (ok) {
+      buttonIcon.style.transform = `rotate(45deg)`;
+    } else {
+      buttonIcon.style.transform = "none";
+    }
+  };
+
+  const itemClick = (event) => {
+    if (!event.target.id) {
+      return;
+    }
+    const target = event.path[1];
+    const clickCheck = target.style.textDecoration;
+    if (clickCheck === "line-through") {
+      target.style.textDecoration = "none";
+      target.style.color = "inherit";
+    } else {
+      target.style.textDecoration = "line-through";
+      target.style.color = "darkgray";
+    }
   };
 
   const onSubmit = (event) => {
     event.preventDefault();
     const inputValue = inputRef.current.childNodes[0][0].value;
-    setTodoInput(inputValue);
+    const listBox = document.createElement("div");
+    listBox.setAttribute(
+      "style",
+      `display: flex; align-items: center; width: 200px; `
+    );
+    listBox.innerHTML = `
+      <input type="checkbox" id=${inputValue} />
+      <h6>${inputValue}</h6>
+    `;
+    checkBoxRef.current.appendChild(listBox);
+    inputRef.current.childNodes[0][0].value = "";
   };
 
   return (
     <div className={styles.todolist}>
-      <div>{todoInput}</div>
+      <div className={styles.list}>
+        <div ref={checkBoxRef} className={styles.checkboxList}></div>
+      </div>
       <div ref={inputRef} className={`${styles.inputForm}`}>
         <form onSubmit={onSubmit}>
-          <input type="text" />
+          <input type="text" placeholder="What is your main focus for today?" />
         </form>
       </div>
       <button className={styles.button} onClick={onClick}>
